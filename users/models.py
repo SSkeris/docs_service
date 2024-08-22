27a5cebcdbe17b9e24e33247466ac64a836aea71
Stas <stasfuryus@gmail.com>
2024-08-22 14:27:43 +0300

    create User model; edit settings.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email", help_text="Email address")
+    #  по заданию этого достаточно, я мог бы добавить не нужные поля информационного характера, но не вижу в этом смысла
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
