101bf0d0711257f460a8be6a70aaab3d63391861
Stas <stasfuryus@gmail.com>
2024-08-22 16:46:21 +0300

    edit docs.views.py

--- docs/views.py
+++ docs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Document
+from .serializers import DocumentSerializer
 
-# Create your views here.
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        # отправка уведомления администратору реализована с использованием Celery
