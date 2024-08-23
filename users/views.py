2eee05a585a42cb399b3a07bd23e0350cb6076f3
Stas <stasfuryus@gmail.com>
2024-08-23 13:01:06 +0300

    edit admin.py in docs, users

--- users/views.py
+++ users/views.py
@@ -7,5 +7,5 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-    #  параметры доступа не указаны, прикрыл авторизацией, предполагая,
+    #  параметры доступа не указаны в тз, прикрыл авторизацией, предполагая,
     #  что программа написана не для общественного доступа, а внутри организации
